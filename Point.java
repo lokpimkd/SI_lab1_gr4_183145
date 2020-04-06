5a2503be3f3dc7d2c1235240345b6bfa9de8f25a
unknown <bojan1233@gmail.com>
2020-04-07 00:07:50 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -1,21 +1,45 @@
 class Point {
 	String id, boja;
-	double x,y;
-
-	//TODO add new variable
-
-	//TODO constructor
-
-	//TODO setters and getters
-
-	public void move (char xDirection, char yDirection) {
-		//TODO
+	static double x;
+	static double y;
+	public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+	public static void setX(double u){
+        x = u;
+    }
+    public static void setY(double z){
+        y = z;
+    }
+	public static double getX() {
+        return x;
+    }
+	public static double getY() {
+		return y;
+	}
+	public static void move (char xDirection, char yDirection) {
+		double x = getX();
+		double y = getY();
+		if(xDirection=='L') {
+			double nova = x-1.0;
+			setX(nova);
+		}
+		if(xDirection=='R') {
+			double nova = x+1.0;
+			setX(nova);
+		}
+		if(yDirection=='U') {
+			double nova = y+1.0;
+			setY(nova);
+		}
+		if(xDirection=='D') {
+			double nova = x-1.0;
+			setY(nova);
+		}
 	}
 
-	public void draw () {
-		//TODO
+	public static void draw () {
+		System.out.println( "("+ getX()+","+getY()+")");
 	}
-
-
-
 }
